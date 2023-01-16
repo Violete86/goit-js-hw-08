cebc6b7f12ca57f940e8e8312e3db7d278a3d147
Violete86 <115045476+Violete86@users.noreply.github.com>
2023-01-16 08:06:57 +0200

    get frameEl

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,3 +8,7 @@ const player = new Player('handstick', {
 player.on('play', function() {
     console.log('played the video!');
 });
+
+const frameEl = document.querySelector('#vimeo-player');
+
+frameEl.addEventListener('', saveTime)
