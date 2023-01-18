c55c17308488919fdb756ffc9f6be2bf25e82264
Violete86 <115045476+Violete86@users.noreply.github.com>
2023-01-18 18:52:53 +0200

    get frameEl, player, LOCALSTORAGE

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,12 @@
 import Player from '@vimeo/player';
 
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
-
-player.on('play', function() {
-    console.log('played the video!');
-});
-
 const frameEl = document.querySelector('#vimeo-player');
 
-frameEl.addEventListener('', saveTime)
+const player = new Player(frameEl);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', function(e) {
+    console.log(e);
+});
+
