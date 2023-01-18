ea016b51074458f6a0e572da5ee13b53ce40b425
Violete86 <115045476+Violete86@users.noreply.github.com>
2023-01-18 21:44:44 +0200

    finish 2d task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const frameEl = document.querySelector('#vimeo-player');
 
@@ -6,7 +7,16 @@ const player = new Player(frameEl);
 
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
-player.on('timeupdate', function(e) {
-    console.log(e);
-});
+player.on('timeupdate', throttle(time => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+}, 1000)
+);
+let startTime = 0;
+let getTime = localStorage.getItem('videoplayer-current-time');
+
+if(getTime) {
+    const parsedGetTime = JSON.parse(getTime);
+    startTime = parsedGetTime.seconds;
+}
+player.setCurrentTime(startTime);
 
